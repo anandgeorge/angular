2fb50e362d7e287fb2075854221f45a8f757bffe
Anand George <mranandgeorge@gmail.com>
2014-01-16 13:21:02 +0530

    Route params

---
+++ hello-routeparams/public/js/controller.js
@@ -0,0 +1,18 @@
+var sampleApp = angular.module('myApp', []);
+ 
+sampleApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/ShowLang/:lang', {
+    templateUrl: 'templates/lang.html',
+    controller: 'ShowLangController'
+      });
+}]);
+ 
+ 
+sampleApp.controller('ShowLangController', function($scope, $routeParams) {
+	$scope.languages = {"English":"Hello World", "German":"Hallo Welt", "French":"Bonjour tout le monde"};
+    $scope.lang = $routeParams.lang;
+	$scope.say = $scope.languages[$routeParams.lang];
+});
+
